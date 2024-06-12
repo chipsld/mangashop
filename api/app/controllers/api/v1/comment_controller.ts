f6347324065be967869b3ec85f7e7ca6ef8f051b
Etienne <etienne@kinoba.fr>
2024-06-12 17:48:18 +0200

    feat:  add finished comment_controller

--- api/app/controllers/api/v1/comment_controller.ts
+++ api/app/controllers/api/v1/comment_controller.ts
@@ -5,20 +5,19 @@ export default class CommentController {
     const { id } = params
     return Comment.findByOrFail('manga_id', id)
   }
+
   async store({ request }: HttpContext) {
     return Comment.create({
       text: request.params.name,
     })
   }
 
-  /**
-   * Handle the form submission to update a specific post by id
-   */
-  async update({ params, request }: HttpContext) {}
+  async update({ params, request }: HttpContext) {
+    const comment = await Comment.findOrFail(params.id)
+    comment.merge(request.all())
+    await comment.save()
+  }
 
-  /**
-   * Handle the form submission to delete a specific post by id.
-   */
   async destroy({ params }: HttpContext) {
     const user = await Comment.findOrFail(params.id)
     await user.delete()
