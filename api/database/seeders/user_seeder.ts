3837b935243a8195ff5ff93a4ae1db3f1a7ee266
Etienne <etienne@kinoba.fr>
2024-06-12 16:20:37 +0200

    fix: user seeder use user factory

--- api/database/seeders/user_seeder.ts
+++ api/database/seeders/user_seeder.ts
@@ -2,11 +2,7 @@ import { UserFactory } from '#database/factories/user_factory'
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 
 export default class extends BaseSeeder {
-    async run() {
-
-        return await genresClient.getMangaGenres().then((response: JikanResponse<mangaType>) => {
-            return response.data
-          })
-    await .createMany(5)
+  async run() {
+    await UserFactory.createMany(5)
   }
 }
