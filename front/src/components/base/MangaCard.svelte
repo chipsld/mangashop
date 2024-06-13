2dcbcd045bd26739b440591f847cdfe6fd69684e
MarcGotze <marc@kinoba.fr>
2024-06-13 16:39:57 +0200

    navbar stylized

--- front/src/components/base/MangaCard.svelte
+++ front/src/components/base/MangaCard.svelte
@@ -4,13 +4,13 @@
 
 <div class="w-60 bg-white border border-gray-200 rounded-lg shadow">
 
-    <img class="rounded-t-lg w-full object-cover" src={manga.images.webp.largeImageUrl} alt="" />
+    <img class="rounded-t-lg w-full object-cover" src={manga.cover} alt="" />
 
     <div class="p-5">
         <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{manga.title}</h5>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">{manga.title}</h5>
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{manga.authors[0].name}</p>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{manga.author}</p>
         <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Read more
              <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
